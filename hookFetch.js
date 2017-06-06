c4b02da06e7abfbd0f8165778286bf97e818e30a
gooyie <zhengzh1003@gmail.com>
2017-06-06 11:43:37 +0800

    使用 webrtc datachannel 传输 fetch 流

--- hookFetch.js
+++ hookFetch.js
@@ -1,8 +1,139 @@
 /*
   Code Source: https://github.com/spacemeowx2/DouyuHTML5Player/blob/b5a54240f1b31d53a8530af83444b10027fe6dca/src/hookfetch.js
 */
+
+let channelId = 0;
+let localConnection;
+
+class StreamReader {
+
+  constructor(port) {
+    this._channelId = channelId++ & 0x3ff;
+    let dataChannel = localConnection.createDataChannel('dataChannel', {id: this._channelId});
+    this._dataChannel = dataChannel;
+    this._port = port;
+    this._cansend = false;
+    this._onCanSend = () => this._cansend = true;
+
+    dataChannel.onopen = this._onopen.bind(this);
+    dataChannel.onclose = this._onclose.bind(this);
+    dataChannel.onerror = this._onerror.bind(this);
+    dataChannel.onmessage = this._onmessage.bind(this);
+  }
+
+  read() {
+    return new Promise((resolve, reject) => {
+      this._readCallback = resolve;
+      this._errorCallback = reject;
+      this._send('read');
+    })
+  }
+
+  cancel() {
+    return new Promise((resolve, reject) => {
+      this._cancelCallback = resolve;
+      this._errorCallback = reject;
+      this._send('cancel');
+    })
+  }
+
+  _onopen(event) {
+    console.log(`[dataChannel] opened data channel ${this._channelId}`);
+  }
+
+  _onclose(event) {
+    console.log(`[dataChannel] closed data channel ${this._channelId}`);
+  }
+
+  _send(...args) {
+    if (this._cansend) {
+      this._dataChannel.send(...args);
+    } else {
+      this._onCanSend = () => {
+        this._cansend = true;
+        this._dataChannel.send(...args);
+      };
+    }
+  }
+
+  _onmessage(event) {
+    // console.log('[dataChannel] received', event.data);
+    let data = event.data;
+
+    if (data instanceof ArrayBuffer) {
+      this._readCallback({done: false, value: new Uint8Array(data)});
+    } else if (data === 'done') {
+      this._dataChannel.close();
+      this._readCallback({done: true, value: undefined});
+    } else if (data === 'canceled') {
+      this._dataChannel.close();
+      this._cancelCallback();
+    } else if (data === 'remoteOpened') {
+      this._port('stream', [this._channelId]).then(this._onCanSend);
+    }
+  }
+
+  _onerror(event) {
+    console.error(`[dataChannel] error of data channel ${this._channelId}`, event);
+    this._errorCallback(new Error(event));
+  }
+
+}
+
+function createConnection() {
+  let port = chrome.runtime.connect({name: 'signaling'});
+
+  let localConnection = new RTCPeerConnection();
+  let initChannel = localConnection.createDataChannel('initChannel', {id: channelId++ & 0x3ff});
+
+  window.addEventListener('unload', () => {
+    localConnection.close();
+    port.disconnect();
+  });
+
+  localConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      port.postMessage(event.candidate.toJSON());
+    }
+  }
+
+  initChannel.onopen = (event) => {
+    console.log('[initChannel] opened', event);
+    initChannel.close();
+  }
+
+  initChannel.onclose = (event) => {
+    console.log('[initChannel] closed', event);
+  }
+
+  port.onMessage.addListener((msg = {}) => {
+    console.log('[signaling]', msg);
+      try {
+        if (msg.status && msg.status === 'connected') {
+          localConnection.createOffer().then(offer => {
+            port.postMessage(offer.toJSON());
+            localConnection.setLocalDescription(offer);
+          });
+        } else if (msg.type && msg.type === 'answer') {
+          localConnection.setRemoteDescription(new RTCSessionDescription(msg));
+        } else if (msg.candidate) {
+          localConnection.addIceCandidate(new RTCIceCandidate(msg));
+        }
+      } catch (err) {
+        console.error(err.stack);
+      }
+  });
+
+  port.onDisconnect.addListener(port => {
+    console.log('[signaling] disconnected');
+  });
+
+  return localConnection;
+}
+
 if( isChrome && location.protocol=='https:' ){
   console.log('chrome+https环境，替换fetch');
+  localConnection = createConnection();
 
 (function () {
   let self = this
@@ -66,18 +197,20 @@ if( isChrome && location.protocol=='https:' ){
       r.headers = convertHeader(r.headers)
       r.body = {
         getReader () {
-          return {
-            read () {
-              return requireReader(() => port('reader.read')).then(r => {
-                if(r.value!=undefined)
-                	r.value = new Uint8Array(r.value)
-                return r
-              })
-            },
-            cancel () {
-              return requireReader(() => port('reader.cancel'))
-            }
-          }
+          console.log('[getReader]', args[0])
+          return new StreamReader(port)
+          // return {
+            // read () {
+              // return requireReader(() => port('reader.read')).then(r => {
+                // if(r.value!=undefined)
+                  // r.value = new Uint8Array(r.value)
+                // return r
+              // })
+            // },
+            // cancel () {
+              // return requireReader(() => port('reader.cancel'))
+            // }
+          // }
         }
       }
       return r
--- hookFetch_background.js
+++ hookFetch_background.js
@@ -1,6 +1,93 @@
 /*
   fetch hooking code from https://github.com/spacemeowx2/DouyuHTML5Player/blob/b5a54240f1b31d53a8530af83444b10027fe6dca/src/background.js#L8
 */
+
+let connections = new Map();
+let dataChannels = new WeakMap();
+
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name !== 'signaling') return;
+
+  const tabId = port.sender.tab.id;
+  console.log(`[${tabId}][signaling] connected`);
+  let remoteConnection;
+
+  port.onMessage.addListener((msg = {}) => {
+    console.log(`[${tabId}][signaling]`, msg);
+    (async () => {
+      try {
+        if (msg.type && msg.type === 'offer') {
+          remoteConnection = new RTCPeerConnection();
+          connections.set(tabId, remoteConnection);
+          console.log(`updated peer connections`, connections);
+          remoteConnection.onicecandidate = (event) => {
+            if (event.candidate) {
+                port.postMessage(event.candidate.toJSON());
+            }
+          };
+
+          remoteConnection.ondatachannel = (event) => {
+            if (event.channel.label === 'initChannel') {
+              console.log(`[${tabId}][ondatachannel] got init channel`);
+              let initChannel = event.channel;
+              initChannel.onopen = (event) => {
+                console.log(`[${tabId}][initChannel] opened`);
+              };
+              initChannel.onclose = (event) => {
+                console.log(`[${tabId}][initChannel] closed`);
+              };
+            } else if (event.channel.label === 'dataChannel') {
+              let dataChannel = event.channel;
+              let id = event.channel.id;
+              console.log(`[${tabId}][ondatachannel] got data channel ${id}`);
+
+              if (dataChannels.has(remoteConnection)) {
+                dataChannels.get(remoteConnection).set(id, event.channel);
+                console.log(`[${tabId}] updated data channels`, dataChannels.get(remoteConnection));
+              } else {
+                dataChannels.set(remoteConnection, new Map([[id, event.channel]]));
+                console.log(`[${tabId}] updated data channels`, dataChannels.get(remoteConnection));
+              }
+
+              dataChannel.onopen = (event) => {
+                console.log(`[${tabId}][dataChannel] opened data channel ${id}`);
+                event.currentTarget.send('remoteOpened');
+              };
+              dataChannel.onclose = (event) => {
+                  console.log(`[${tabId}][dataChannel] closed data channel ${id}`);
+              };
+              dataChannel.onerror = (event) => {
+                console.error(`[${tabId}][dataChannel] error of data channel ${id}`, event);
+              };
+            }
+          };
+
+          await remoteConnection.setRemoteDescription(new RTCSessionDescription(msg));
+          let answer = await remoteConnection.createAnswer();
+          port.postMessage(answer.toJSON());
+          await remoteConnection.setLocalDescription(answer);
+        } else if (msg.candidate) {
+          await remoteConnection.addIceCandidate(new RTCIceCandidate(msg));
+        }
+      } catch (err) {
+        console.error(err.stack);
+      }
+    })();
+  });
+
+  port.onDisconnect.addListener(port => {
+    console.log(`[${tabId}][signaling] disconnected`);
+    if (connections.has(tabId)) {
+      let connection = connections.get(tabId);
+      connections.delete(tabId);
+      console.info(`[${tabId}][deleted peer connection]`, connection);
+      console.log(`updated peer connections`, connections);
+    }
+  });
+
+  port.postMessage({status: 'connected'});
+});
+
 function convertHeader(headers) {
   let out = {}
   for (let key of headers.keys()) {
@@ -41,6 +128,37 @@ chrome.runtime.onConnect.addListener(port => {
         })
       } else if (msg.method === 'json') {
         chain = chain.then(() => response.json())
+      } else if (msg.method === 'stream') {
+        reader = response.body.getReader();
+
+        chain = chain.then(() => {
+          let tabId = port.sender.tab.id;
+          let channelId = msg.args[0];
+          let dataChannel = dataChannels.get(connections.get(tabId)).get(channelId);
+
+          dataChannel.onmessage = (event) => {
+            // console.log(`[${tabId}][dataChannel] data channel ${channelId} received`, event.data);
+            let channel = event.currentTarget;
+
+            if (event.data === 'read') {
+              reader.read().then(r => {
+                if (r.done) {
+                  channel.send('done');
+                  dataChannels.get(connections.get(tabId)).delete(channelId);
+                  console.log(`[${tabId}] updated data channels`, dataChannels.get(connections.get(tabId)));
+                } else {
+                  channel.send(r.value);
+                }
+              })
+            } else if (event.data === 'cancel') {
+              reader.cancel().then(() => {
+                channel.send('canceled');
+                dataChannels.get(connections.get(tabId)).delete(channelId);
+                console.log(`[${tabId}] updated data channels`, dataChannels.get(connections.get(tabId)));
+              });
+            }
+          };
+        });
       } else if (msg.method === 'body.getReader') {
         chain = chain.then(() => {
           reader = response.body.getReader()
